d75c800aa97ec82ada42241a30fc60a41272eb4c
brian <rainy.kuchiki@yahoo.com>
2016-08-10 20:01:43 +0800

    modified:   .classpath 	modified:   bin/net/brian/jdk/notes/effectivejava/item01/JDBCDemo.class 	modified:   bin/net/mindview/io/Worm.class 	modified:   bin/net/mindview/util/Enums.class 	modified:   src/net/brian/jdk/notes/effectivejava/item01/JDBCDemo.java 	modified:   src/net/mindview/util/Enums.java
    	.gitignore
    	externalLibs/commons-logging-1.1.3.jar
    	src/net/brian/jdk/notes/tij/enumeration/

--- src/net/brian/jdk/notes/effectivejava/item01/JDBCDemo.java
+++ src/net/brian/jdk/notes/effectivejava/item01/JDBCDemo.java
@@ -2,11 +2,16 @@ package net.brian.jdk.notes.effectivejava.item01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCDemo {
 	public static void getJDBCConnection() {
 		try {
+			// This Driver class here is the service provider interface.
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -16,16 +21,34 @@ public class JDBCDemo {
 		String password = "bqy@Home2016";
 		// Connection is a service interface right here.
 		Connection conn = null;
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		String sql = "select name from user where gender=?";
 		try {
-			// public static Connection getConnection(String url, String user, String password)
+			// public static Connection getConnection(String url, String user,
+			// String password)
 			// We have many other getConnection()s for service access api.
-			// DriverManager, as a provider registration api, will implement Connection interface in different ways.
+			// DriverManager, as a provider registration api, will implement
+			// Connection interface in different ways.
 			conn = DriverManager.getConnection(url, username, password);
+
+			List advs = new ArrayList();
+			if (conn != null) {
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, "M");
+				rs = stmt.executeQuery();
+				if (rs != null) {
+					while (rs.next()) {
+						System.out.println("JDBCDemo -- getJDBCConnection() -- resultSet:: " + rs.getString(1));
+					}
+				}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		System.out.println("JDBCDemo -- getJDBCConnection() -- conn:: " + conn);
 		try {
+			stmt.close();
 			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
